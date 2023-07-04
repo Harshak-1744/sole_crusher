f0224d03b325ed6fb68a167d0929592143435890
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-07-04 23:02:37 +0530

    Update sole_crusher.java

--- sole_crusher.java
+++ sole_crusher.java
@@ -6,6 +6,7 @@ public class sole_crusher
 {
     public static void main(String[] args) {
         ArrayList<String> swear_string = new ArrayList<>();
+         swear_string.add("You cant win..!");
         swear_string.add("Your programming abilities are so outdated, they should be stored in a museum.");
         swear_string.add("The complexity of your code is truly mesmerizing—to the point of inducing sleep.");
         swear_string.add("Congratulations on achieving the bare minimum. You must be so proud.");
@@ -13,8 +14,8 @@ public class sole_crusher
         swear_string.add("Your project's success rate is like dividing by zero — a guaranteed crash.");
         swear_string.add("Your problem-solving skills are like a never-ending loop of incompetence.");
         swear_string.add("Your programming abilities are so outdated, they should be stored in a museum.");
-        swear_string.add("You cant win..!");
         swear_string.add("Your skills are truly one in a billion. Too bad there are billions of others.");
+        swear_string.add("You're a master of inconsistency. Your work is consistently disappointing.");
 
         Collections.shuffle(swear_string);
 
