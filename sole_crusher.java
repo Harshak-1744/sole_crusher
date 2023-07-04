24f1d7e8a54507e9eab56c78623fa40e15a16472
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-07-04 22:24:34 +0530

    Add files via upload

---
+++ sole_crusher.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.util.ArrayList;
+
+public class sole_crusher
+{
+    public static void main(String [] args)
+    {
+        ArrayList<String> swear_string = new ArrayList<>();
+        swear_string.add("Your programming abilities are so outdated, they should be stored in a museum.");
+        swear_string.add("The complexity of your code is truly mesmerizing—to the point of inducing sleep.");
+        swear_string.add("Congratulations on achieving the bare minimum. You must be so proud.");
+        swear_string.add("Your code is so advanced, it's practically back in the Stone Age.");
+        swear_string.add("Your project's success rate is like dividing by zero—a guaranteed crash.");
+        swear_string.add("Your problem-solving skills are like a never-ending loop of incompetence.");
+        swear_string.add("Your programming abilities are so outdated, they should be stored in a museum.");
+        swear_string.add("You cant win..!");
+        
+        for(String i : swear_string)
+        {
+            System.out.println(i);
+        }
+    }
+}
